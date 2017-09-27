5098ea410a2ff64a746f39b3dde1dd2913320c9f
chibi929 <ibihc929@gmail.com>
2017-09-27 22:38:30 +0900

    Add tool bar module.

--- src/app/module/app-material.module.ts
+++ src/app/module/app-material.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule,
          MdCheckboxModule,
-         MdIconModule } from '@angular/material';
+         MdIconModule,
+         MdToolbarModule } from '@angular/material';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
-    MdIconModule
+    MdIconModule,
+    MdToolbarModule
   ],
   exports: [
     BrowserAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
-    MdIconModule
+    MdIconModule,
+    MdToolbarModule
   ]
 })
 export class AppMaterialModule { }
