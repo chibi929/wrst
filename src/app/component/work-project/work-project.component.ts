19210757636fb82a14d12e3e01eb0a9b63589ae1
chibi929 <ibihc929@gmail.com>
2017-10-12 06:57:31 +0900

    Modify work.

--- src/app/component/work-project/work-project.component.ts
+++ src/app/component/work-project/work-project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Work } from '../../entity/work';
 
 @Component({
   selector: 'work-project',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-project.component.scss']
 })
 export class WorkProjectComponent implements OnInit {
+  private readonly works: Work[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.addWorkElement();
   }
 
+  ngOnInit(): void {
+  }
+
+  private addWorkElement(): void {
+    this.works.push(new Work())
+  }
+
+  private onDeleteButtonClicked(i: number): void {
+    if (this.works.length === 1) {
+      return;
+    }
+    this.works.splice(i, 1);
+  }
+
+  private onAddedButtonClicked(): void {
+    this.addWorkElement();
+  }
 }
