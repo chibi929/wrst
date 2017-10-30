b8872a37f3ca4e17fb7b9354cf20038697710bb2
chibi929 <ibihc929@gmail.com>
2017-10-30 23:19:25 +0900

    Implement 'free-space'.

--- src/app/component/free-space/free-space.component.ts
+++ src/app/component/free-space/free-space.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Free  } from '../../entity/free';
 
 @Component({
   selector: 'free-space',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./free-space.component.scss']
 })
 export class FreeSpaceComponent implements OnInit {
+  private readonly frees: Free[] = [];
 
-  constructor() { }
+  constructor() {
+    this.addElement();
+  }
 
   ngOnInit() {
   }
 
+  private addElement(): void {
+    this.frees.push(new Free())
+  }
+
+  private onDeleteButtonClicked(i: number): void {
+    if (this.frees.length === 1) {
+      return;
+    }
+    this.frees.splice(i, 1);
+  }
+
+  private onAddedButtonClicked(): void {
+    this.addElement();
+  }
 }
